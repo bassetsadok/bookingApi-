380bd228785ebe83b2d3713b8ba3f4f8978ee79a
bassetsadok <salah.sadok67@gmail.com>
2022-03-26 13:37:39 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+const port = 3000;
+app.listen(3000, () => {
+  console.log(`app is running on port ${3000}...`);
+});
